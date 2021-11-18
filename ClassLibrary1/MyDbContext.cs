396f21b7d39ad40eabde72b1b83c2b8b6941f9ab
Michael Röth <michael.roeth@audi.de>
2021-11-18 09:50:56 +0100

    Projektdateien hinzufügen.

---
+++ ClassLibrary1/MyDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ClassLibrary1
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+
+
+           
+        }
+
+        public virtual DbSet<MySet> MySet { get; set; }
+
+        public void DoEvilStuff()
+        {
+            using (var con = Database.GetDbConnection())
+            {
+                con.Open();
+                foreach (var entityType in Model.GetEntityTypes())
+                {
+                    var xx = entityType.ClrType.Name;
+                }
+
+            }
+        }
+    }
+
+    public class MySet
+    {
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary1;
+
+Console.WriteLine("Hello, World!");
+
+var mydb = new MyDbContext();
+mydb.DoEvilStuff();
